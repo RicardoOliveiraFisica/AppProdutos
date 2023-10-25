bfc8bf38b41a275e9590a8a2d97aa6e8bca81aee
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-25 12:39:48 -0300

    mapeamento para o BD

--- AppProdutos/src/main/java/br/com/ro/AppProdutos/model/Estoque.java
+++ AppProdutos/src/main/java/br/com/ro/AppProdutos/model/Estoque.java
@@ -2,9 +2,28 @@ package br.com.ro.AppProdutos.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estoque")
 public class Estoque {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "produto_id", referencedColumnName = "id")
 	private Produto produto;
+	
+	@Column(nullable = false)
 	private Integer quantidade;
 	
 	public Estoque() {}
