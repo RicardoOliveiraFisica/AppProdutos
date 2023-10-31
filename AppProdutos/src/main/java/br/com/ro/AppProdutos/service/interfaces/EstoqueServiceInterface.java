fe9464afc135ce27b10797e0f3231f0945119ed3
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-31 12:46:24 -0300

    atualizacao

--- AppProdutos/src/main/java/br/com/ro/AppProdutos/service/interfaces/EstoqueServiceInterface.java
+++ AppProdutos/src/main/java/br/com/ro/AppProdutos/service/interfaces/EstoqueServiceInterface.java
@@ -9,7 +9,7 @@ public interface EstoqueServiceInterface {
 	Estoque save(Estoque estoque);
 	Optional<Estoque> getById(Long id);
 	List<Estoque> getAll();
-	Estoque update(Estoque produto);
+	Estoque update(Estoque estoque);
 	void delete(Long id);
 	Estoque addQuantidade(Long id, int quantidade);
 	Estoque delQuantidade(Long id, int quantidade);
